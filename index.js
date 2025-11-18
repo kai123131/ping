2b93a0e9f10958c57f1af8ee4fd0a81fcb30950e
kai123131 <pransh.jain@classroom.gdgoenkasurat.com>
2025-11-18 17:27:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-client.login(TOKEN);
-require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
+// Example ping command
 client.on("messageCreate", msg => {
-  if (msg.content === "ping") {
-    msg.reply("🏓 pong!");
+  if (msg.content === "!ping") {
+    msg.reply("Pong!");
   }
 });
 
+// LOGIN using environment variable
 client.login(process.env.TOKEN);
